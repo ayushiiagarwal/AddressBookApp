839b36cd56716398e7a9f563eca227180db12488
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2026-03-09 12:10:00 +0530

    [Ayushi] Add .Added UC-1 Create contact

--- addressbook-app/src/main/java/com/addressbook/controller/AddressBookController.java
+++ addressbook-app/src/main/java/com/addressbook/controller/AddressBookController.java
@@ -1,5 +1,20 @@
 package com.addressbook.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.addressbook.model.Contact;
+
+@RestController
 public class AddressBookController {
-    
+    @GetMapping("/contacts")
+    public List<Contact> getAllContacts(){
+        Contact c1 = new Contact(01, "Olive", "9123658974");
+        Contact c2 = new Contact(02, "Adam", "9854721451");
+
+        return Arrays.asList(c1, c2);
+    }
 }
---
+++ addressbook-app/src/main/java/com/addressbook/model/Contact.java
@@ -0,0 +1,18 @@
+package com.addressbook.model;
+
+public class Contact {
+    private int id;
+    private String name, phone;
+
+    public Contact(int id, String name, String phone){
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId(){ return id; }
+
+    public String getName(){ return name; }
+
+    public String getPhone(){ return phone; }
+}
