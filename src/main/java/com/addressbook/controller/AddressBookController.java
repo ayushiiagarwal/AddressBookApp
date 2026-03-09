0c189c6223dba2ea9cf6b1154282801fff0ed835
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2026-03-09 15:10:53 +0530

    [Ayushi] Add .Added UC-1 Create Contact

--- src/main/java/com/addressbook/controller/AddressBookController.java
+++ src/main/java/com/addressbook/controller/AddressBookController.java
@@ -1,20 +1,18 @@
 package com.addressbook.controller;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.addressbook.model.Contact;
 
 @RestController
+@RequestMapping("/addressbook")
 public class AddressBookController {
-    @GetMapping("/contacts")
-    public List<Contact> getAllContacts(){
-        Contact c1 = new Contact("Adam", "Connor", "NY-101", "New York City", "New York", "4555184", "9587463256");
-        Contact c2 = new Contact("Olive", "Smith", "PR-154", "Paris", "France", "5544475", "7458963214");
 
-        return Arrays.asList(c1, c2);
+    @PostMapping("/create")
+    public Contact createContact(@RequestBody Contact contact){
+        return contact;
     }
 }
